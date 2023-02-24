2e24de8549a3c02672f15c90eaa533e9bc6c862b
Karol Moroz <karol@moroz.dev>
2023-02-24 23:16:34 +0100

    Set up prettier and layouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import path from "path";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "@styles": path.resolve(process.cwd(), "src/styles"),
+        "@layouts": path.resolve(process.cwd(), "src/layouts"),
+        "@components": path.resolve(process.cwd(), "src/components"),
+      },
+    },
+  },
+});
